b8d95fb77109ec18e969d3ca70de83f70d46c152
Jack Savage <jacksavage@users.noreply.github.com>
2024-04-01 04:00:35 +0000

    Add configuration

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -8,14 +8,5 @@ Invoke-WebRequest -Uri https://aka.ms/getwingetpreview -OutFile $wingetInstallFi
 Add-AppPackage -Path $wingetInstallFile
 Remove-Item -Path $wingetInstallFile
 
-# stop if winget not available
-# https://learn.microsoft.com/en-us/windows/package-manager/winget/
-Get-Command -Name winget -ErrorAction Stop
-
-# install 1pass
-winget install --id AgileBits.1Password.CLI --source winget --exact
-winget install --id AgileBits.1Password --source winget --exact
-
-# add my account and sign in
-op account add
-
+# apply configuration
+winget configure --file ./configurations/configuration.dsc.yaml
