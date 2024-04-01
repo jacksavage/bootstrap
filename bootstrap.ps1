18bbe916a5ac666850bddd9ef2c2f5cd7fe8718a
Jack Savage <jacksavage@users.noreply.github.com>
2024-04-01 03:47:49 +0000

    Add winget preview installer

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,26 @@
+# todo create restore point
+
+# install winget preview to get access to experimental features
+# note: this method of install does not support auto updates
+# https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-preview-version-developers-only
+$wingetInstallFile = "winget.msixbundle"
+Invoke-WebRequest -Uri https://aka.ms/getwingetpreview -OutFile $wingetInstallFile
+Add-AppPackage -Path $wingetInstallFile
+Remove-Item -Path $wingetInstallFile
+
+# enable winget "configure" feature
+$wingetSettingsFile =
+winget settings export `
+| ConvertFrom-Json `
+| Select-Object -ExpandProperty userSettingsFile
+$wingetSettings =
+  Get-Content -Path $wingetSettingsFile -Raw `
+  | ConvertFrom-Json
+$wingetSettings.experimentalFeatures.configuration03 = $true
+$wingetSettings `
+| ConvertTo-Json -Depth 10 `
+| Set-Content -Path $wingetSettingsFile
+
 # stop if winget not available
 # https://learn.microsoft.com/en-us/windows/package-manager/winget/
 Get-Command -Name winget -ErrorAction Stop
